46ac1236a696d611adfdfebd79fdf873cf95c402
tyler <tyler.r.hood@gmail.com>
2018-12-02 12:10:38 -0800

    added function to handle Errors caught by the .catch

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/searches', createSearch);
 
 function Book(info) {
     const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
-    
+
     this.title = info.volumeInfo.title || 'No title available';
     this.isbn = info.volumeInfo.industryIdentifiers[0].identifier || info.volumeInfo.industryIdentifiers[1].identifier || 'No ISBN available';
     this.image_url = info.volumeInfo.imageLinks.smallThumbnail|| placeholderImage;
@@ -41,7 +41,12 @@ function createSearch(request, response) {
     if (request.body.searchRadio === 'author') { url += `+inauthor:${request.body.searchTerm}&maxResults=20`; }
   
     superagent.get(url)
-      .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult)))
-      .then(results => response.render('pages/searches/show', {searchResults: results}))
-      .catch(error => handleError(error, response));
-  }
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult)))
+    .then(results => response.render('pages/searches/show', {searchResults: results}))
+    .catch(error => handleError(error, response));
+}
+
+function handleError(error, response) {
+    console.log(error);
+    response.render('pages/error.ejs', {error: 'Uh Oh'});
+}
