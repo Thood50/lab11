f76510ac6d6b3fe7724aff6766d3caad6dc9203d
tyler <tyler.r.hood@gmail.com>
2018-12-02 12:01:56 -0800

    added callback function for post request

--- server.js
+++ server.js
@@ -21,3 +21,15 @@ app.post('/searches', createSearch);
 function getHome (request, response) {
     return response.render('pages/index.ejs');
 }
+
+function createSearch(request, response) {
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  
+    if (request.body.searchRadio === 'title') { url += `+intitle:${request.body.searchTerm}&maxResults=20`; }
+    if (request.body.searchRadio === 'author') { url += `+inauthor:${request.body.searchTerm}&maxResults=20`; }
+  
+    superagent.get(url)
+      .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult)))
+      .then(results => response.render('pages/searches/show', {searchResults: results}))
+      .catch(error => handleError(error, response));
+  }
